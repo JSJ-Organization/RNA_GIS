13185661caff97b4bb107c4824235d309c25b66b
SUNGJUN1234 <chotjdwns654@naver.com>
2024-06-28 17:59:26 +0900

    #8 - Input 상호작용 추가

--- frontend/src/components/Input.jsx
+++ frontend/src/components/Input.jsx
@@ -1,19 +1,69 @@
+import { useRef, useState, useEffect } from 'react';
 
 const Input = () => {
-  return (
-    <div id="input_container">
-        <input className="c-checkbox" type="checkbox" id="checkbox" />
-        <div className="c-formContainer">
-            <div className="c-form">
-                <input className="c-form__input" placeholder="주소입력" type="text" required />
-                <label className="c-form__buttonLabel" htmlFor="checkbox">
-                  <button className="c-form__button" type="button">검색</button>
-                </label>
-                <label className="c-form__toggle" htmlFor="checkbox" data-title="좌표 변환기 👆"></label>
-            </div>
-        </div> 
-    </div>
-  )
-}
+  const [formVisible, setFormVisible] = useState(false);
+  const inputRef = useRef(null);
+  const buttonRef = useRef(null);
 
-export default Input
+  // 라벨 클릭 시 input 포커스 및 폼 표시
+  const handleLabelClick = () => {
+    setFormVisible(true);
+  };
+
+  useEffect(() => {
+    if (formVisible) {
+      setTimeout(() => {
+        if (inputRef.current) {
+          inputRef.current.focus();
+        }
+      }, 200);
+    }
+  }, [formVisible]);
+
+  // input에서 엔터 클릭 시 버튼 클릭
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter' && buttonRef.current) {
+      sendAddress();
+    }
+  };
+
+  // 주소 정보 전송
+  const sendAddress = () => {
+    if (inputRef.current) {
+      console.log(inputRef.current.value);
+    }
+  };
+
+  return (
+    <div id="top-container">
+      <div className={`c-form-container ${formVisible ? 'form-visible' : ''}`}>
+        <div className="c-form">
+          <input
+            className="c-form-input"
+            placeholder="주소입력"
+            type="text"
+            required
+            ref={inputRef}
+            onKeyDown={handleKeyPress}
+          />
+          <div className="c-form-button-label" onClick={sendAddress}>
+            <button
+              className="c-form-button"
+              type="button"
+              ref={buttonRef}
+            >
+              검색
+            </button>
+          </div>
+          <div
+            className="c-form-welcome"
+            data-title="좌표 변환기 👆"
+            onClick={handleLabelClick}
+          ></div>
+        </div>
+      </div> 
+    </div>
+  );
+};
+
+export default Input;
