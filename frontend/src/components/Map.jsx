96137a7ca750d821192eedf8134af8699748c65e
SUNGJUN1234 <chotjdwns654@naver.com>
2024-07-01 21:13:44 +0900

    #17 - OpenLayers 지도 표출

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import { useEffect, useRef } from 'react'
+import { Map as OlMap, View } from 'ol';
+import { fromLonLat } from 'ol/proj';
+import TileLayer from 'ol/layer/Tile';
+import { OSM } from 'ol/source';
+import { defaults } from 'ol/control/defaults';
+
 
 const Map = () => {
   const location = useLocation();
@@ -9,13 +16,31 @@ const Map = () => {
   const x = queryParams.get('x');
   const y = queryParams.get('y');
 
+  const mapRef = useRef(null)
+  useEffect(() => {
+    const tilelayer = new TileLayer({
+        source: new OSM({ attributions: '' })
+    });
+    const view = new View({
+        center: fromLonLat([x, y]), // 서울시청 경도와 위도
+        zoom: 16
+    })
+  
+    const map = new OlMap({
+        controls: defaults({ zoom: false, rotate: false, attribution: false }),
+        layers: [
+            tilelayer
+        ],
+        view: view,
+    })
+    map.setTarget(mapRef.current || '')
+    return () => {
+        map.setTarget('');
+    };
+}, []);
   return (
-    <div>
-      <h1>Map Coordinates</h1>
-      <p>X: {x}</p>
-      <p>Y: {y}</p>
-    </div>
-  );
+  <div id='map' ref={mapRef} />
+)
 }
 
 export default Map;
