597892973366205675def9454eae57e582badbd7
Min-SungJo <123or@naver.com>
2024-07-04 02:06:14 +0900

    #7 - RestClient 이름 변경
    JusoApiClientConfig
    -> RestClientConfig

--- backend/src/main/java/com/jsj/backend/config/RestClientConfig.java
+++ backend/src/main/java/com/jsj/backend/config/RestClientConfig.java
@@ -5,17 +5,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 
 /**
- * Juso API 클라이언트를 위한 구성 클래스.
- * 이 클래스는 RestClient 빈을 정의하여 Juso API와 통신하는 데 사용됩니다.
+ * HTTP Client 를 위한 구성 클래스.
+ * 이 클래스는 RestClient 빈을 정의하여 HTTP 통신에 사용됩니다.
  *
  * @Configuration 애노테이션을 사용하여 스프링 컨텍스트에서 구성 클래스로 인식됩니다.
  */
 @Configuration
-public class JusoApiClientConfig {
+public class RestClientConfig {
 
     /**
      * RestClient 빈 정의.
-     * 이 메서드는 Juso API와 통신하기 위해 RestClient 객체를 생성합니다.
+     * 이 메서드는 API와 통신하기 위해 RestClient 객체를 생성합니다.
      *
      * @return 설정된 RestClient 객체
      */
