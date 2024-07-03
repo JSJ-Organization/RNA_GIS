786243f1c02454e8c6971b1fb15be0190b333642
SUNGJUN1234 <chotjdwns654@naver.com>
2024-07-04 04:46:59 +0900

    #17 - 공공데이터 제 1유형 이미지 추가

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import img_opentype01 from '../assets/images/img_opentype01.png'
 
 const Footer = () => {
   return (
-    <div>
+    <div id='footer-top'>
+      <img src={img_opentype01} alt="공공누리 마크 제 1유형" />
     </div>
   )
 }
