66d76a935841e05155a6b0e363dc178e6f475ba6
Min-SungJo <123or@naver.com>
2024-07-04 17:10:20 +0900

    #33 - fe-be 컨테이너화

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-  },
+  // server: { 개발 서버 설정
+  //   host: '0.0.0.0',
+  //   port: 80,
+  //   strictPort: true,
+  // },
 })
+
