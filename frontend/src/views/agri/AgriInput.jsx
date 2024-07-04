29e3a624c5430786acef21cc3fa572ae800b81d9
SUNGJUN1234 <chotjdwns654@naver.com>
2024-07-04 10:00:08 +0900

    #31 - hook을 활용한 css 변수 설정

--- frontend/src/views/agri/AgriInput.jsx
+++ frontend/src/views/agri/AgriInput.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import Input from '../../components/Input'
 import { Helmet } from 'react-helmet-async'
+import useCssVariables from '../../hooks/useCssVariables'
 
 const AgriInput = () => {
+    useCssVariables({
+        '--coordinate100': '#1bff73',
+        '--coordinate200': '#b0ffb4',
+        '--coordinate300': '#A8FFC9',
+      });
+
   return (
     <>
         <Helmet>
--- frontend/src/views/coord/CoordInput.jsx
+++ frontend/src/views/coord/CoordInput.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import Input from '../../components/Input'
 import { Helmet } from 'react-helmet-async'
+import useCssVariables from '../../hooks/useCssVariables';
 
 const CoordInput = () => {
+
+    useCssVariables({
+        '--coordinate100': '#ff7b73',
+        '--coordinate200': '#ffaea9',
+        '--coordinate300': '#ffcccc',
+      });
+
   return (
     <>
         <Helmet>
