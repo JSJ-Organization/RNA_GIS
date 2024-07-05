4a41a95080e446a11c8b8f487b6fd106e3b8e329
SUNGJUN1234 <chotjdwns654@naver.com>
2024-07-05 20:37:50 +0900

    #17 - 검색 반경 표시

--- frontend/src/components/map/AgriMap.jsx
+++ frontend/src/components/map/AgriMap.jsx
@@ -1,5 +1,5 @@
 import { useLocation } from 'react-router-dom';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Map as OlMap, View } from 'ol';
 import { fromLonLat } from 'ol/proj';
 import TileLayer from 'ol/layer/Tile';
@@ -9,12 +9,16 @@ import VectorLayer from 'ol/layer/Vector';
 import VectorSource from 'ol/source/Vector';
 import Feature from 'ol/Feature';
 import Point from 'ol/geom/Point';
-import { Style, Icon } from 'ol/style';
+import Circle from 'ol/geom/Circle';
+import { Style, Icon, Fill, Stroke } from 'ol/style';
 import LayerSwitcher from 'ol-layerswitcher';
+import Group from 'ol/layer/Group';
 import 'ol-layerswitcher/src/ol-layerswitcher.css';
 import markerImg from '../../assets/images/marker.png';
+import { useLoading } from '../../contexts/LoadingContext';
 
 const AgriMap = () => {
+  
   const location = useLocation();
 
   // URLSearchParams를 사용하여 쿼리 파라미터를 파싱
@@ -22,47 +26,114 @@ const AgriMap = () => {
   const x = parseFloat(queryParams.get('x'));
   const y = parseFloat(queryParams.get('y'));
 
+  const { showLoading, hideLoading } = useLoading();
+  const [radius, setRadius] = useState(0);
+
   const mapRef = useRef(null);
 
+  const getAgriData = async (distance) => {
+    try {
+      showLoading();
+      const url = `http://localhost:8080/api/v1/search/office?query=s&x=${x}&y=${y}&distance=${distance}`;
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      setRadius(distance / 2);
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      hideLoading();
+    }
+  };
+
   useEffect(() => {
+    getAgriData(2000);
+  }, [x, y]);
+
+  useEffect(() => {
+    if (radius === 0) return;
 
     // Vworld 지도 기본 레이어 api 호출
-    const vworldLayer  = new TileLayer({
+    const vworldLayer = new TileLayer({
       source: new XYZ({
-        url : `http://localhost:8080/api/v1/search/wmts/Base/{z}/{y}/{x}.png`,
-      })
-    })
+        url: `http://localhost:8080/api/v1/search/wmts/Base/{z}/{y}/{x}.png`,
+      }),
+    });
 
     // 마커 생성
     const marker = new Feature({
-      geometry: new Point(fromLonLat([x, y]))
+      geometry: new Point(fromLonLat([x, y])),
     });
 
     // 마커 스타일 설정
-    marker.setStyle(new Style({
-      image: new Icon({
-        src: markerImg,
-        scale: 1,
-        anchor: [0.5, 1],
-        anchorXUnits: 'fraction',
-        anchorYUnits: 'fraction',
+    marker.setStyle(
+      new Style({
+        image: new Icon({
+          src: markerImg,
+          scale: 1,
+          anchor: [0.5, 1],
+          anchorXUnits: 'fraction',
+          anchorYUnits: 'fraction',
+        }),
       })
-    }));
+    );
+
+    // 원 생성 (반경 : raidus미터)
+    const circle = new Feature({
+      geometry: new Circle(fromLonLat([x, y]), radius),
+    });
+
+    // 원 스타일 설정
+    circle.setStyle(
+      new Style({
+        stroke: new Stroke({
+          color: 'rgba(0, 0, 255, 0.5)',
+          width: 2,
+        }),
+        fill: new Fill({
+          color: 'rgba(0, 0, 255, 0.1)',
+        }),
+      })
+    );
 
     // 벡터 소스 및 레이어 생성
     const vectorSource = new VectorSource({
-      features: [marker]
+      features: [marker],
     });
 
     const markerLayer = new VectorLayer({
+      title: '내 위치 마커',
+      source: vectorSource,
+    });
+
+    const circleSource = new VectorSource({
+      features: [circle],
+    });
+
+    const circleLayer = new VectorLayer({
+      title: '반경 표시',
+      source: circleSource,
+    });
+
+    // 마커 그룹 생성
+    const markerGroup = new Group({
       title: '마커',
-      source: vectorSource
+      layers: [markerLayer],
+    });
+
+    // 반경 그룹 생성
+    const circleGroup = new Group({
+      title: '반경',
+      layers: [circleLayer],
     });
 
     // 지도 뷰 설정
     const view = new View({
       center: fromLonLat([x, y]),
-      zoom: 16
+      zoom: 16,
     });
 
     // 레이어 스위처 생성
@@ -73,12 +144,11 @@ const AgriMap = () => {
       groupSelectStyle: 'children', // Can be 'children' [default], 'group' or 'none'
     });
 
-
     // 지도 생성
     const map = new OlMap({
       controls: defaultControls().extend([layerSwitcher]),
-      layers: [vworldLayer, markerLayer],
-      view: view
+      layers: [vworldLayer, markerGroup, circleGroup],
+      view: view,
     });
 
     map.setTarget(mapRef.current || '');
@@ -86,11 +156,13 @@ const AgriMap = () => {
     return () => {
       map.setTarget('');
     };
-  }, [x, y]);
+  }, [radius, x, y]);
 
   return (
+    <div>
       <div id='map' ref={mapRef} />
+    </div>
   );
-}
+};
 
 export default AgriMap;
