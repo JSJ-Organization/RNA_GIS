86b87e7a75f84fb69aa9ba9e44cb675ee26d2fee
SUNGJUN1234 <chotjdwns654@naver.com>
2024-07-06 16:28:35 +0900

    #31 - 모달창 정보 토글기능

--- frontend/src/components/modal/AgriModal.jsx
+++ frontend/src/components/modal/AgriModal.jsx
@@ -2,9 +2,23 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMapLocationDot } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { useState, useRef, useEffect } from 'react';
 
 const AgriModal = ({ modalVisible, selectedResult, closeModal }) => {
-    return (
+
+  const [showHasAgri, setShowHasAgri] = useState(false);
+  const [agriHeight, setAgriHeight] = useState('0px');
+  const agriContentRef = useRef(null);
+
+  const toggleHasAgri = () => {
+    setShowHasAgri(!showHasAgri);
+  };
+
+  useEffect(() => {
+    setAgriHeight(showHasAgri ? `${agriContentRef.current.scrollHeight}px` : '0px');
+  }, [showHasAgri]);
+
+  return (
     <div>
       <div onClick={closeModal} className={`modal-window ${modalVisible ? 'modal-visible' : ''}`}>
         {selectedResult && (
@@ -12,61 +26,69 @@ const AgriModal = ({ modalVisible, selectedResult, closeModal }) => {
             <div onClick={closeModal} className="modal-close">Close</div>
             <div className='address'>
               <span>
-                {selectedResult.institutionNm }
+                {selectedResult.institutionNm}
               </span>
             </div>
             <div>
               <span>
-                전화번호 : {selectedResult.phoneNumber }
+                전화번호 : {selectedResult.phoneNumber}
               </span>
             </div>
-            <div>
-              <span>
-                트랙터 : {selectedResult.trctorHoldCo } 대
-              </span>
+            <div className='toggle-button' onClick={toggleHasAgri}>
+              <span >보유 농기계 정보 {showHasAgri? '▲' : '▼' }</span>
             </div>
-            <div>
-              <span>
-                경운기 : {selectedResult.cultvtHoldCo } 대
-              </span>
-            </div>
-            <div>
-              <span>
-                관리기 : {selectedResult.manageHoldCo } 대
-              </span>
-            </div>
-            <div>
-              <span>
-                땅속 작물 수확기 : {selectedResult.harvestHoldCo } 대
-              </span>
-            </div>
-            <div>
-              <span>
-                탈곡기 및 정선 작업기 : {selectedResult.thresherHoldCo } 대
-              </span>
-            </div>
-            <div>
-              <span>
-                자주형 파종기 : {selectedResult.planterHoldCo } 대
-              </span>
-            </div>
-            <div>
-              <span>
-                이앙 작업기 : {selectedResult.planterHoldCo } 대
-              </span>
-            </div>
-            <div>
-              <span>
-                벼 수확 및 운반 작업기 : {selectedResult.planterHoldCo } 대
-              </span>
-            </div>
-            <div>
-              <span>
-                기타 임대 농기계 : {selectedResult.planterHoldCo } 대
-              </span>
+            <div 
+              ref={agriContentRef} 
+              className="has-agri-container" 
+              style={{ height: agriHeight, transition: 'height 0.3s ease' }}>
+              <div>
+                <span>
+                  트랙터 : {selectedResult.trctorHoldCo} 대
+                </span>
+              </div>
+              <div>
+                <span>
+                  경운기 : {selectedResult.cultvtHoldCo} 대
+                </span>
+              </div>
+              <div>
+                <span>
+                  관리기 : {selectedResult.manageHoldCo} 대
+                </span>
+              </div>
+              <div>
+                <span>
+                  땅속 작물 수확기 : {selectedResult.harvestHoldCo} 대
+                </span>
+              </div>
+              <div>
+                <span>
+                  탈곡기 및 정선 작업기 : {selectedResult.thresherHoldCo} 대
+                </span>
+              </div>
+              <div>
+                <span>
+                  자주형 파종기 : {selectedResult.planterHoldCo} 대
+                </span>
+              </div>
+              <div>
+                <span>
+                  이앙 작업기 : {selectedResult.planterHoldCo} 대
+                </span>
+              </div>
+              <div>
+                <span>
+                  벼 수확 및 운반 작업기 : {selectedResult.planterHoldCo} 대
+                </span>
+              </div>
+              <div>
+                <span>
+                  기타 임대 농기계 : {selectedResult.planterHoldCo} 대
+                </span>
+              </div>
             </div>
             <Link to={`/agricultural/map?x=${selectedResult.x}&y=${selectedResult.y}`}>
-              <div className='map-Link'>
+              <div className='map-link'>
                 <FontAwesomeIcon icon={faMapLocationDot} /> 주변 농기계 임대 정보 보기
               </div>
             </Link>
@@ -78,16 +100,20 @@ const AgriModal = ({ modalVisible, selectedResult, closeModal }) => {
 }
 
 AgriModal.propTypes = {
-    modalVisible: PropTypes.bool.isRequired,
-    selectedResult: PropTypes.shape({
-        roadNameAddress: PropTypes.string,
-        parcelAddress: PropTypes.string,
-        zipcode: PropTypes.number,
-        x: PropTypes.number,
-        y: PropTypes.number
-    }),
-    closeModal: PropTypes.func.isRequired
+  modalVisible: PropTypes.bool.isRequired,
+  selectedResult: PropTypes.shape({
+    institutionNm: PropTypes.string,
+    phoneNumber: PropTypes.string,
+    trctorHoldCo: PropTypes.string,
+    cultvtHoldCo: PropTypes.string,
+    manageHoldCo: PropTypes.string,
+    harvestHoldCo: PropTypes.string,
+    thresherHoldCo: PropTypes.string,
+    planterHoldCo: PropTypes.string,
+    x: PropTypes.number,
+    y: PropTypes.number,
+  }),
+  closeModal: PropTypes.func.isRequired,
 };
-
 
 export default AgriModal;
