20776851ecd0ec88a2265a4bac7a1467b2cbe4a1
민성조 <91239439+Min-SungJo@users.noreply.github.com>
2024-07-14 23:20:01 +0900

    #51 - .sh Nginx 설정 파일 확인 (디버깅용)

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -3,5 +3,9 @@
 # 환경 변수를 치환하여 Nginx 설정 파일 생성
 envsubst '${DOMAIN}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
+# Nginx 설정 파일 확인 (디버깅용)
+echo "Nginx configuration:"
+cat /etc/nginx/conf.d/default.conf
+
 # Nginx 시작
 exec nginx -g 'daemon off;'
